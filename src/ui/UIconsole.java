3433693f24f6bec8fb72218aaa5a04a41ae1e5fc
Valeriya <valeriyavino@gmail.com>
2019-03-20 23:40:08 +0900

    Changed menu

--- src/ui/UIconsole.java
+++ src/ui/UIconsole.java
@@ -11,19 +11,12 @@ public class UIconsole implements UI {
         if (a.equals("1")){
             System.out.println("Input login");
             System.out.println("Input password");
-        } else if (a.equals("2")) {
-            System.out.println("1-Ladiescloth 2-Mencloth 3-back");
-
+        }else if (a.equals("2")){
+            System.out.println("1-Women's clothing 2-Men's clothing 3-back 4-show Basket 5-to buy");
         }else if (a.equals("3")){
             System.out.println("We will be glad to see you again!");
-            return;
         }
-
-        }
-
-
-
-
+    }
 
     @Override
     public void end() {
