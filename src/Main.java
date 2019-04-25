4f746ea0c5b277b414e9baf3410b929379a28aa3
Valeriya <valeriyavino@gmail.com>
2019-04-25 14:11:45 +0900

    Editing

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import ui.UIconsole;
 
 public class Main {
     public static void main(String[] args) {
-        ActionStore action= new ActionStore(new UIconsole(),new InConsole());
+        ActionStore action = new ActionStore(new UIconsole(), new InConsole());
         action.newProduct();
         action.running();
     }
