e05b28defede0d7fe34fa4b94e7b2322d9905827
Valeriya <valeriyavino@gmail.com>
2019-04-25 14:08:59 +0900

    Array was replaced by ArrayList

--- src/basket/Basket.java
+++ src/basket/Basket.java
@@ -2,27 +2,38 @@ package basket;
 
 import product.Product;
 
-import java.util.Arrays;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Basket {
-    private Product[] products = new Product[100];
+    private List<Product> basket = new ArrayList<>();
+    private LocalDate dateOfShopping = LocalDate.now();
 
-    public Product[] getProducts() {
-        return products;
+    public Basket() {
     }
 
-    public void setProducts(Product product) {
-        for (int i = 0; i < products.length; i++) {
-            if (products[i]==null) {
-                products[i] = product;
-            }
-        }
+    public Basket(List<Product> basket) {
+        this.basket = basket;
+    }
+
+    public List<Product> getBasket() {
+        return basket;
+    }
+
+    public LocalDate getDateOfShopping() {
+        return dateOfShopping;
+    }
+
+    public void setBasket(List<Product> basket) {
+        this.basket = basket;
     }
 
     @Override
     public String toString() {
         return "Basket{" +
-                "products=" + Arrays.toString(products) +
+                "basket=" + basket +
+                ", dateOfShopping=" + dateOfShopping +
                 '}';
     }
 }
